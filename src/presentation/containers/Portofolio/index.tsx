2549ed707e32c0f934f950cb78f5309a67ccce28
Ian Maulana <ian_maulana@yahoo.com>
2023-12-13 23:03:01 +0700

    Feat: Create portofolio

--- src/presentation/containers/Portofolio/index.tsx
+++ src/presentation/containers/Portofolio/index.tsx
@@ -15,32 +15,96 @@ const Portofolio: React.FC<Props> = () => {
       {
         id: 1,
         category: 'Website',
-        title: t('this_example_projects_label'),
-        description: `Lorem ipsum dolor sit amet quo ei simul congue exerci ad nec admodum perfecto.`,
-        imageUrl:
-          'https://jthemes.net/themes/html/bolby/demo/images/works/1.svg',
-        ActionUrl: 'https://google.com',
+        title: 'ianmaulana.me',
+        description: `Website personal pribadi saya yang berisikan portofolio, informasi dsb`,
+        imageUrl: 'https://ianmaulana.sirv.com/ianmaulana.me/ianmaulana.png',
+        actionUrl: 'https://ianmaulana.me',
       },
       {
         id: 2,
-        category: 'Mobile App',
-        title: t('this_example_projects_label'),
-        description: `Lorem ipsum dolor sit amet quo ei simul congue exerci ad nec admodum perfecto.`,
+        category: 'Website',
+        title: 'Sekolah Mengemudi Mentari',
+        description: `Company Profile dari PT Mentari Prima Persada yang digunakan untuk pemasaran usahanya`,
         imageUrl:
-          'https://jthemes.net/themes/html/bolby/demo/images/works/3.svg',
-        ActionUrl: 'https://google.com',
+          'https://ianmaulana.sirv.com/ianmaulana.me/sekolahmengemudimentari.png',
+        actionUrl: 'https://sekolahmengemudimentari.com',
       },
       {
         id: 3,
         category: 'Website',
-        title: t('this_example_projects_label'),
-        description: `Lorem ipsum dolor sit amet quo ei simul congue exerci ad nec admodum perfecto.`,
+        title: 'Amanda Hartanto Batik',
+        description: `Company Profile sekaligus web penjualan untuk usaha butik milak amanda hartanto`,
+        imageUrl: 'https://ianmaulana.sirv.com/ianmaulana.me/amandabutik.png',
+        actionUrl: 'https://amandahartantobatik.com',
+      },
+      {
+        id: 4,
+        category: 'Website',
+        title: 'Backoffice P2P',
+        description: `Backoffice P2P yang digunakan oleh tim internal untuk mereview borrower`,
         imageUrl:
-          'https://jthemes.net/themes/html/bolby/demo/images/works/6.svg',
-        ActionUrl: 'https://google.com',
+          'https://ianmaulana.sirv.com/ianmaulana.me/backoffice-dompetkilat.png',
+        actionUrl: '',
+      },
+      {
+        id: 5,
+        category: 'Website',
+        title: 'Borrower Dashboard P2P',
+        description:
+          'Website client (borrower) untuk melihat history, dan melakukan pengajuan pinjaman',
+        imageUrl:
+          'https://ianmaulana.sirv.com/ianmaulana.me/borrower-dashboard.png',
+        actionUrl: '',
+      },
+      {
+        id: 6,
+        category: 'Website',
+        title: 'Inventory Sistem',
+        description:
+          'Sistem yang digunakan untuk memanajemen pembelian, penjualan dan stok barang',
+        imageUrl:
+          'https://ianmaulana.sirv.com/ianmaulana.me/inventory-system.png',
+        actionUrl: '',
+      },
+      {
+        id: 7,
+        category: 'Website',
+        title: 'Koperasi Sistem',
+        description: 'Sistem yang digunakan untuk management usaha koperasi',
+        imageUrl:
+          'https://ianmaulana.sirv.com/ianmaulana.me/koperasi-system.png',
+        actionUrl: '',
+      },
+      {
+        id: 8,
+        category: 'Mobile Apps',
+        title: 'Koperasi Mobile',
+        description:
+          'Sistem yang digunakan anggota koperasi untuk memantau pinjaman, simpanan dsb',
+        imageUrl:
+          'https://ianmaulana.sirv.com/ianmaulana.me/koperasi-mobile.png',
+        actionUrl: '',
+      },
+      {
+        id: 9,
+        category: 'Mobile Apps',
+        title: 'Lender App',
+        description:
+          'Aplikasi yang digunakan untuk mendanai (P2P) pinjaman yang telah disetujui',
+        imageUrl: 'https://ianmaulana.sirv.com/ianmaulana.me/lender-app.png',
+        actionUrl: '',
+      },
+      {
+        id: 10,
+        category: 'Mobile Apps',
+        title: 'Actionpay',
+        description:
+          'Aplikasi yang digunakan untuk melakukan transaksi transfer ke luar negeri',
+        imageUrl: 'https://ianmaulana.sirv.com/ianmaulana.me/actionpay.png',
+        actionUrl: '',
       },
     ];
-  }, [t]);
+  }, []);
 
   return (
     <>
